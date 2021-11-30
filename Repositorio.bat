fe75a1a9012f88dcacdd2b80283fb650e850050b
Facundo DO <80721922+facu231@users.noreply.github.com>
2021-11-29 22:51:43 -0300

    Se suben todas las modificaciones

--- Repositorio.bat
+++ Repositorio.bat
@@ -3,9 +3,7 @@ estado de git
 git add .
 git commit -m " Se suben todas las modificaciones " 
 git branch -M main
-git remoto agregar origen https://github.com/facu231/Proyec.git
 git push -u origen principal
-git remoto agregar origen https://github.com/facu231/Proyec.git
 git branch -M main
 git push -u origen principal
 
