2ed30876e604942d9e8b2c3ca19d37bb77a8a68a
Facundo DO <80721922+facu231@users.noreply.github.com>
2021-11-29 22:50:13 -0300

    Se suben todas las modificaciones

--- Repositorio.bat
+++ Repositorio.bat
@@ -1,8 +1,12 @@
-echo "Proyec"
 git init
-git status 
+estado de git 
 git add .
-git commit -m'Se suben Modificaciones'
+git commit -m " Se suben todas las modificaciones " 
 git branch -M main
-git push -u origin main
+git remoto agregar origen https://github.com/facu231/Proyec.git
+git push -u origen principal
+git remoto agregar origen https://github.com/facu231/Proyec.git
+git branch -M main
+git push -u origen principal
+
 pause
